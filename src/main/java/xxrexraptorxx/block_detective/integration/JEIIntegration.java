5c61391343f240e47cc29ef52b366109ea4d8944
XxRexRaptorxX <Raptor-Simon@web.de>
2022-08-22 22:50:49 +0200

    backport to MC 1.18.x

--- src/main/java/xxrexraptorxx/block_detective/integration/JEIIntegration.java
+++ src/main/java/xxrexraptorxx/block_detective/integration/JEIIntegration.java
@@ -6,7 +6,7 @@ import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.runtime.IIngredientManager;
 import net.minecraft.ChatFormatting;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
@@ -32,14 +32,14 @@ public class JEIIntegration implements IModPlugin {
         if (Config.ENABLE_JEI_ITEM_PAGES.get()) {
             for (Block block : ForgeRegistries.BLOCKS) {
 
-                registry.addIngredientInfo(new ItemStack(block), VanillaTypes.ITEM_STACK, Component.literal(ForgeRegistries.ITEMS.getKey(block.asItem()).toString())
-                    .append(Component.literal("\n\nDestroy Time: " + String.valueOf(block.defaultDestroyTime())))
-                    .append(Component.literal("\nJump Factor: " + String.valueOf(block.getJumpFactor())))
-                    .append(Component.literal("\nSpeed Factor: " + String.valueOf(block.getSpeedFactor())))
-                    .append(Component.literal("\nLight Level: " + String.valueOf(block.defaultBlockState().getLightEmission())))
-                    .append(Component.literal("\nFriction: " + String.valueOf(block.getFriction())))
-                    .append(Component.literal("\nFlammable: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isFlammable())))
-                    .append(Component.literal("\nSolid: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isSolid())))
+                registry.addIngredientInfo(new ItemStack(block), VanillaTypes.ITEM, new TextComponent(ForgeRegistries.ITEMS.getKey(block.asItem()).toString())
+                    .append(new TextComponent("\n\nDestroy Time: " + String.valueOf(block.defaultDestroyTime())))
+                    .append(new TextComponent("\nJump Factor: " + String.valueOf(block.getJumpFactor())))
+                    .append(new TextComponent("\nSpeed Factor: " + String.valueOf(block.getSpeedFactor())))
+                    .append(new TextComponent("\nLight Level: " + String.valueOf(block.defaultBlockState().getLightEmission())))
+                    .append(new TextComponent("\nFriction: " + String.valueOf(block.getFriction())))
+                    .append(new TextComponent("\nFlammable: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isFlammable())))
+                    .append(new TextComponent("\nSolid: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isSolid())))
                         .withStyle(ChatFormatting.DARK_GRAY));
         }
         }
--- src/main/java/xxrexraptorxx/block_detective/utils/Events.java
+++ src/main/java/xxrexraptorxx/block_detective/utils/Events.java
@@ -4,9 +4,7 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.*;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -26,8 +24,8 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.VersionChecker;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
-import xxrexraptorxx.block_detective.main.References;
 import xxrexraptorxx.block_detective.main.BlockDetective;
+import xxrexraptorxx.block_detective.main.References;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -51,17 +49,17 @@ public class Events {
                 if (ForgeRegistries.ITEMS.getKey(item) == ForgeRegistries.BLOCKS.getKey(block)) {
 
                     if (!Screen.hasShiftDown()) {
-                        list.add(Component.translatable("message.block_detective.hold_shift.desc").withStyle(ChatFormatting.GRAY));
+                        list.add(new TranslatableComponent("message.block_detective.hold_shift.desc").withStyle(ChatFormatting.GRAY));
 
                     } else {
-                        if (Config.SHOW_REGISTRY_NAME.get()) list.add(Component.literal(ForgeRegistries.ITEMS.getKey(item).toString()).withStyle(ChatFormatting.GOLD));
-                        if (Config.SHOW_DESTROY_TIME.get()) list.add(Component.literal("Destroy Time: " + String.valueOf(block.defaultDestroyTime())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_JUMP_FACTOR.get()) list.add(Component.literal("Jump Factor: " + String.valueOf(block.getJumpFactor())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_SPEED_FACTOR.get()) list.add(Component.literal("Speed Factor: " + String.valueOf(block.getSpeedFactor())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_LIGHT_LEVEL.get()) list.add(Component.literal("Light Level: " + String.valueOf(block.defaultBlockState().getLightEmission())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_FRICTION.get()) list.add(Component.literal("Friction: " + String.valueOf(block.getFriction())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_IF_FLAMMABLE.get()) list.add(Component.literal("Flammable: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isFlammable())).withStyle(ChatFormatting.YELLOW));
-                        if (Config.SHOW_IF_SOLID.get()) list.add(Component.literal("Solid: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isSolid())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_REGISTRY_NAME.get()) list.add(new TextComponent(ForgeRegistries.ITEMS.getKey(item).toString()).withStyle(ChatFormatting.GOLD));
+                        if (Config.SHOW_DESTROY_TIME.get()) list.add(new TextComponent("Destroy Time: " + String.valueOf(block.defaultDestroyTime())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_JUMP_FACTOR.get()) list.add(new TextComponent("Jump Factor: " + String.valueOf(block.getJumpFactor())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_SPEED_FACTOR.get()) list.add(new TextComponent("Speed Factor: " + String.valueOf(block.getSpeedFactor())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_LIGHT_LEVEL.get()) list.add(new TextComponent("Light Level: " + String.valueOf(block.defaultBlockState().getLightEmission())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_FRICTION.get()) list.add(new TextComponent("Friction: " + String.valueOf(block.getFriction())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_IF_FLAMMABLE.get()) list.add(new TextComponent("Flammable: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isFlammable())).withStyle(ChatFormatting.YELLOW));
+                        if (Config.SHOW_IF_SOLID.get()) list.add(new TextComponent("Solid: " + FormattingHelper.ConvertBooleanToString(block.defaultBlockState().getMaterial().isSolid())).withStyle(ChatFormatting.YELLOW));
                     }
                 }
             }
@@ -80,10 +78,10 @@ public class Events {
                 if (VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.OUTDATED ||
                         VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.BETA_OUTDATED ) {
 
-                    MutableComponent url = Component.literal(ChatFormatting.GREEN + "Click here to update!");
+                    MutableComponent url = new TextComponent(ChatFormatting.GREEN + "Click here to update!");
                     url.withStyle(url.getStyle().withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, References.URL)));
 
-                    Minecraft.getInstance().player.displayClientMessage(Component.literal(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"), false);
+                    Minecraft.getInstance().player.displayClientMessage(new TextComponent(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"), false);
                     Minecraft.getInstance().player.displayClientMessage(url, false);
 
                     hasShownUp = true;
@@ -103,7 +101,7 @@ public class Events {
      */
     @SubscribeEvent
     public static void SupporterRewards(PlayerEvent.PlayerLoggedInEvent event) {
-        Player player = event.getEntity();
+        Player player = event.getPlayer();
         Level world = player.getLevel();
 
         if (Config.PATREON_REWARDS.get()) {
@@ -123,7 +121,7 @@ public class Events {
                         //test if player is supporter
                         if (SupporterCheck(SUPPORTER_URL, player)) {
 
-                            ItemStack certificate = new ItemStack(Items.PAPER).setHoverName((Component.literal("Thank you for supporting me in my work!").withStyle(ChatFormatting.GOLD).append(Component.literal(" - XxRexRaptorxX").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GREEN))));
+                            ItemStack certificate = new ItemStack(Items.PAPER).setHoverName((new TextComponent("Thank you for supporting me in my work!").withStyle(ChatFormatting.GOLD).append(new TextComponent(" - XxRexRaptorxX").withStyle(ChatFormatting.ITALIC).withStyle(ChatFormatting.GREEN))));
 
                             CompoundTag ownerNBT = new CompoundTag();
                             ItemStack reward = new ItemStack(Items.PLAYER_HEAD);
@@ -137,7 +135,7 @@ public class Events {
 
                         //test if player is premium supporter
                         if (SupporterCheck(PREMIUM_SUPPORTER_URL, player)) {
-                            ItemStack reward = new ItemStack(Items.DIAMOND_SWORD, 1).setHoverName(Component.literal("Rex's Night Sword").withStyle(ChatFormatting.DARK_GRAY));
+                            ItemStack reward = new ItemStack(Items.DIAMOND_SWORD, 1).setHoverName(new TextComponent("Rex's Night Sword").withStyle(ChatFormatting.DARK_GRAY));
                             reward.enchant(Enchantments.MENDING, 1);
                             reward.enchant(Enchantments.SHARPNESS, 3);
                             player.addItem(reward);
@@ -145,7 +143,7 @@ public class Events {
 
                         //test if player is elite
                         if (SupporterCheck(ELITE_URL, player)) {
-                            player.addItem(new ItemStack(Items.NETHER_STAR).setHoverName(Component.literal("Elite Star")));
+                            player.addItem(new ItemStack(Items.NETHER_STAR).setHoverName(new TextComponent("Elite Star")));
                         }
                     }
                 }
